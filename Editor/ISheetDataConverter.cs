2f3646fbd0186e7002d41a5b36e820df9ddd85f3
Dumble009 <daichi19990821@gmail.com>
2023-02-23 17:01:10 +0900

    ISheetDataConverter.Convert関数にsummaryを追加

--- Editor/ISheetDataConverter.cs
+++ Editor/ISheetDataConverter.cs
@@ -5,6 +5,11 @@ namespace googleDriveDownloader
     /// </summary>
     public interface ISheetDataConverter
     {
+        /// <summary>
+        /// シートデータを何らかの構造のテキスト形式に変換し、その文字列を返す
+        /// </summary>
+        /// <param name="sheetData">変換対象のシートデータ。非nullであること</param>
+        /// <returns>sheetDataを変換した結果得られた文字列。そのままファイルへ書き込んだりすることが出来る。</returns>
         string Convert(SheetData sheetData);
     }
 }
