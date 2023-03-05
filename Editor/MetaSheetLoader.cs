49075be10bf4dbca1933284aa59e534a383f855f
Dumble009 <daichi19990821@gmail.com>
2023-03-05 20:42:32 +0900

    MetaSheetLoaderに、メタシートのIDを読み込む関数を追加
    その関数の動作確認を行うテストケースを追加し、パスする事を確認

--- Editor/MetaSheetLoader.cs
+++ Editor/MetaSheetLoader.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Diagnostics;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace GoogleDriveDownloader
@@ -9,7 +10,15 @@ namespace GoogleDriveDownloader
     /// </summary>
     public class MetaSheetLoader
     {
+        /// <summary>
+        /// このソースコードのファイルから設定ファイルまでの相対パス
+        /// </summary>
         const string CONFIG_RELATIVE_PATH = "../Config/config.json";
+
+        /// <summary>
+        /// 設定ファイル中のID情報のキー
+        /// </summary>
+        const string CONFIG_FILE_KEY_ID = "ID";
         public MetaSheetLoader()
         {
 
@@ -31,6 +40,32 @@ namespace GoogleDriveDownloader
             string sourceDirPath = Path.GetDirectoryName(sourceFilePath);
 
             return Path.Combine(sourceDirPath, CONFIG_RELATIVE_PATH);
+        }
+
+        /// <summary>
+        /// 設定ファイルからメタシートのシートIDを読み込んで返す
+        /// </summary>
+        /// <returns>メタシートのシートIDの文字列</returns>
+        private string LoadMetaSheetID()
+        {
+            try
+            {
+                // jsonファイルをテキスト形式で開き、読み込んだ文字列をパースする
+                using (StreamReader file = File.OpenText(GetConfigPath()))
+                {
+                    string jsonString = file.ReadToEnd();
+
+                    var dic = JsonConvert.DeserializeObject
+                                <Dictionary<string, string>>
+                                (jsonString);
+
+                    return dic["ID"];
+                }
+            }
+            catch (System.Exception e)
+            {
+                throw new System.Exception("Loading Meta Sheet ID failed." + e.Message);
+            }
         }
     }
 }
--- Tests/MetaSheetLoaderTest.cs
+++ Tests/MetaSheetLoaderTest.cs
@@ -1,13 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
 using GoogleDriveDownloader;
 
+/// <summary>
+/// メタシートを読み込むクラスMetaSheetLoaderをテストするクラス
+/// </summary>
 public class MetaSheetLoaderTest
 {
+    // private関数を呼び出す際は関数名の文字列が必要になるので、定数で宣言しておく
+
+    /// <summary>
+    /// コンフィグファイルのパスを計算する関数の名前
+    /// </summary>
+    const string FUNC_NAME_GET_CONFIG_PATH = "GetConfigPath";
+
+    /// <summary>
+    /// メタシートのIDを読み込む関数の名前
+    /// </summary>
+    const string FUNC_NAME_LOAD_META_SHEET_ID = "LoadMetaSheetID";
+
+    /// <summary>
+    /// 設定ファイルから読み込まれることが期待されるメタシートのID
+    /// </summary>
+    const string EXPECT_META_SHEET_ID = "1_wHr6TWOv2NeckBCXHOMv7qrnQCcXf3BWyeaf9OTQew";
+
     /// <summary>
     /// テスト対象のオブジェクト
     /// </summary>
@@ -23,18 +39,36 @@ public class MetaSheetLoaderTest
     /// MetaSheetLoaderが正しく設定ファイルのパスを計算できるか確認するテスト
     /// </summary>
     [Test]
-    public void MetaSheetLoaderTestConfigFilePath()
+    public void CalcConfigFilePathTest()
     {
-        // 設定ファイルを読み込む関数はprivate
+        // 設定ファイルのパスを計算する関数はprivate
         object retVal = TestUtil.CallPrivateMethod(
-                                metaSheetLoader,
-                                "GetConfigPath",
-                                null
-                        );
+            metaSheetLoader,
+            "GetConfigPath",
+            null
+        );
 
         string retPath = (string)retVal;
 
         // 返り値が指すパスが存在すればOK
         Assert.True(File.Exists(retPath));
+    }
+
+    /// <summary>
+    /// MetaSheetLoaderが正しく設定ファイル中のメタシートのIDを取得できるか確認するテスト
+    /// </summary>
+    [Test]
+    public void LoadMetaSheetIDTest()
+    {
+        // 設定ファイルを読み込む関数はprivate
+        object retVal = TestUtil.CallPrivateMethod(
+            metaSheetLoader,
+            "LoadMetaSheetID",
+            null
+        );
+
+        string retID = (string)retVal;
+
+        Assert.AreEqual(EXPECT_META_SHEET_ID, retID);
     }
 }
