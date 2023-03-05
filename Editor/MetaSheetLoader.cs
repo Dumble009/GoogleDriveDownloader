133776eda51902a35a012cf42319ae81faa06857
Dumble009 <daichi19990821@gmail.com>
2023-03-05 21:57:24 +0900

    MetaSheetLoaderに、メタシートのデータをまとめて読み込む関数を実装
    この結果は、シートの内容によるので、テストは用意しない

--- Editor/MetaSheetLoader.cs
+++ Editor/MetaSheetLoader.cs
@@ -19,6 +19,32 @@ namespace GoogleDriveDownloader
         /// 設定ファイル中のID情報のキー
         /// </summary>
         const string CONFIG_FILE_KEY_ID = "ID";
+
+        /// <summary>
+        /// メタシートにおける、IDのパラメータ名
+        /// </summary>
+        const string META_SHEET_PARAMETER_NAME_ID = "ID";
+
+        /// <summary>
+        /// メタシートにおける、シートIDのパラメータ名
+        /// </summary>
+        const string META_SHEET_PARAMETER_NAME_SHEET_ID = "ShetID";
+
+        /// <summary>
+        /// メタシートにおける、スプレッドシート内の参照シートの名前のパラメータ名
+        /// </summary>
+        const string META_SHEET_PARAMETER_NAME_SHEET_NAME = "SheetName";
+
+        /// <summary>
+        /// メタシートにおける、シートからダウンロードした結果を保存するパスのパラメータ名
+        /// </summary>
+        const string META_SHEET_PARAMETER_NAME_SAVE_PATH = "SavePath";
+
+        /// <summary>
+        /// メタシートにおける、データの表示名のパラメータ名
+        /// </summary>
+        const string META_SHEET_PARAMETER_NAME_DISPLAY_NAME = "DisplayName";
+
         public MetaSheetLoader()
         {
 
@@ -77,6 +103,41 @@ namespace GoogleDriveDownloader
             // Googleドライブからのシートの読み込みにはSheetLoaderを使用する
             SheetLoader sheetLoader = new SheetLoader();
             return sheetLoader.LoadSheetData(LoadMetaSheetID());
+        }
+
+        /// <summary>
+        /// メタシートを読み込んで、各行のデータを要素に持つリストを返す
+        /// </summary>
+        /// <returns>各行のデータを1つの要素として持つリスト</returns>
+        public List<MetaSheetData> LoadMetaSheet()
+        {
+            List<MetaSheetData> datas = new List<MetaSheetData>();
+
+            // メタシートを読み込んだ結果を辞書として貰い、イテレートする
+            var metaSheetDataDic = LoadMetaSheetAsSheetData().Data;
+
+            foreach (var key in metaSheetDataDic.Keys)
+            {
+                var row = metaSheetDataDic[key];
+
+                int id = int.Parse(row[META_SHEET_PARAMETER_NAME_ID]);
+                string sheetID = row[META_SHEET_PARAMETER_NAME_SHEET_ID];
+                string sheetName = row[META_SHEET_PARAMETER_NAME_SHEET_NAME];
+                string savePath = row[META_SHEET_PARAMETER_NAME_SAVE_PATH];
+                string displayName = row[META_SHEET_PARAMETER_NAME_DISPLAY_NAME];
+
+                datas.Add(
+                    new MetaSheetData(
+                        id,
+                        sheetID,
+                        sheetName,
+                        savePath,
+                        displayName
+                    )
+                );
+            }
+
+            return null;
         }
     }
 }
