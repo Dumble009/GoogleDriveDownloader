087d964e1f446787ef8ebcf7cedbbdfc24b53b4e
Dumble009 <daichi19990821@gmail.com>
2023-04-14 16:32:06 +0900

    SourceCodeLocatorの定義を作成

---
+++ Editor/SourceCodeLocator.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+
+namespace GoogleDriveDownloader
+{
+    /// <summary>
+    /// ソースコードのファイルパスを計算して返す機能を提供する静的クラス
+    /// </summary>
+    public class SourceCodeLocator
+    {
+        /// <summary>
+        /// このメソッドを呼び出したクラスのファイルパスを返す
+        /// </summary>
+        /// <returns>
+        /// このメソッドを呼び出したクラスのファイルパス。
+        /// 絶対パスか相対パスかは保証されない。
+        /// </returns>
+        public static string GetSourceCodePath()
+        {
+            return "";
+        }
+    }
+}
