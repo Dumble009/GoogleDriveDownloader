db6321f102609df4b7a566d30e819336bf1ac555
Dumble009 <daichi19990821@gmail.com>
2023-04-14 16:47:34 +0900

    新しい仕様に合わせてSourceCodeLocatorを修正

--- Editor/SourceCodeLocator.cs
+++ Editor/SourceCodeLocator.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Diagnostics;
 
 namespace GoogleDriveDownloader
@@ -8,15 +9,22 @@ namespace GoogleDriveDownloader
     public class SourceCodeLocator
     {
         /// <summary>
-        /// このメソッドを呼び出したクラスのファイルパスを返す
+        /// このメソッドを呼び出したクラスが格納されているディレクトリのパスを返す。
         /// </summary>
         /// <returns>
-        /// このメソッドを呼び出したクラスのファイルパス。
+        /// このメソッドを呼び出したクラスのファイルが格納されているディレクトリのパス。
         /// 絶対パスか相対パスかは保証されない。
         /// </returns>
         public static string GetSourceCodePath()
         {
-            return "";
+            // スタックトレースを使用して、呼び出し元のファイルパスを取得する
+            var callerFileName = new StackTrace(true)
+                                .GetFrame(1) // 0番がこの関数の呼び出し情報なので、1番のフレームを取得する事で、この関数を呼び出した関数のフレームを取得できる。
+                                .GetFileName();
+
+            var directoryName = Path.GetDirectoryName(callerFileName);
+
+            return directoryName;
         }
     }
 }
