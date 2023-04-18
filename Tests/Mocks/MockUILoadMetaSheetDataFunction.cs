f6307e137a415cf3cbb4c0b845c9d68beab31615
Dumble009 <daichi19990821@gmail.com>
2023-04-18 23:01:41 +0900

    IUILoadMetaSheetDataFunctionsのモッククラスを実装。
    RegisterOnTriggeredで渡されたイベントを呼び出す機能と、PassNewMetaSheetDatasで渡されたMetaSheetDataのリストを外部に公開する機能を持っている

---
+++ Tests/Mocks/MockUILoadMetaSheetDataFunction.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using GoogleDriveDownloader;
+
+/// <summary>
+/// IUILoadMetaSheetDataFunctionのモッククラス
+/// PassNewMetaSheetDatasで渡されたMetaSheetDataのリストを外部に公開する。
+/// また、RegisterOnTriggeredで渡されたイベントを任意のタイミングで呼び出す事が出来る。
+/// </summary>
+public class MockUILoadMetaSheetDataFunction : IUILoadMetaSheetDataFunction
+{
+    List<MetaSheetData> passedMetaSheetDatas;
+    /// <summary>
+    /// PassNewMetaSheetDatas関数で引数として渡されたMetaSheetDataのリスト
+    /// </summary>
+    List<MetaSheetData> PassedMetaSheetDatas
+    {
+        get => passedMetaSheetDatas;
+    }
+
+    UILoadMetaSheetDataFunctionHandler handlers;
+
+    public void PassNewMetaSheetDatas(List<MetaSheetData> metaSheetDatas)
+    {
+        passedMetaSheetDatas = metaSheetDatas;
+    }
+
+    public void RegisterOnTriggered(UILoadMetaSheetDataFunctionHandler _handler)
+    {
+        handlers += _handler;
+    }
+
+    /// <summary>
+    /// メタシートの読み込み指示。
+    /// この関数を呼ぶと、PassedMetaSheetDatasにメタシートのデータのリストが渡されるはず
+    /// </summary>
+    public void Load()
+    {
+        handlers();
+    }
+}
