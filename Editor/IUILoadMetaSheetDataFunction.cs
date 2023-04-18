94aca9a81fc7eb168e79add42f622d0089e0348a
Dumble009 <daichi19990821@gmail.com>
2023-04-18 22:57:47 +0900

    メタシートの読み込み操作時に呼び出されるデリゲートの名称を変更
    ~Handlerという名称の方が一般的かと思われるし、自分の他のプロジェクトでもその名称を使っているため揃えた

--- Editor/IUILoadMetaSheetDataFunction.cs
+++ Editor/IUILoadMetaSheetDataFunction.cs
@@ -5,7 +5,7 @@ namespace GoogleDriveDownloader
     /// <summary>
     /// UIからメタシートの読み込み操作が行われた際に呼び出されるイベントのデリゲート
     /// </summary>
-    public delegate void UILoadMetaSheetDataFunctionDelegate();
+    public delegate void UILoadMetaSheetDataFunctionHandler();
 
     /// <summary>
     /// メタシートのデータを読み込んでUIに反映する機能を提供するインタフェース
@@ -15,10 +15,10 @@ namespace GoogleDriveDownloader
         /// <summary>
         /// メタシートのデータの読み込み操作が行われた際に実行されるイベントを登録する
         /// </summary>
-        /// <param name="e">
+        /// <param name="_handler">
         /// メタシートのデータの読み込み操作が行われた際に実行されるイベント
         /// </param>
-        void RegisterOnTriggered(UILoadMetaSheetDataFunctionDelegate e);
+        void RegisterOnTriggered(UILoadMetaSheetDataFunctionHandler _handler);
 
         /// <summary>
         /// 読み込まれたメタシートのデータを受け取りUIに反映する
