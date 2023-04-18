9b246a4477dc2d88f3acd53d73936f20a771dc83
Dumble009 <daichi19990821@gmail.com>
2023-04-18 23:29:07 +0900

    MetaSheetLoaderAdapterを実装。
    全てのテストが問題無くパスする事を確認

--- Editor/MetaSheetLoaderAdapter.cs
+++ Editor/MetaSheetLoaderAdapter.cs
@@ -6,11 +6,34 @@ namespace GoogleDriveDownloader
     /// </summary>
     public class MetaSheetLoaderAdapter
     {
+        /// <summary>
+        /// UI側の、メタシートの読み込み関連の機能を提供するオブジェクト
+        /// </summary>
+        IUILoadMetaSheetDataFunction ui;
+
+        /// <summary>
+        /// メタシートの読み込み処理を行ってくれるオブジェクト
+        /// </summary>
+        IMetaSheetLoader metaSheetLoader;
         public MetaSheetLoaderAdapter(
             IUILoadMetaSheetDataFunction _ui,
             IMetaSheetLoader _metaSheetLoader
         )
         {
+            ui = _ui;
+            metaSheetLoader = _metaSheetLoader;
+
+            ui.RegisterOnTriggered(this.OnLoad);
+        }
+
+        /// <summary>
+        /// メタシートの読み込み処理がUIから呼び出された際に実行される
+        /// </summary>
+        private void OnLoad()
+        {
+            var datas = metaSheetLoader.LoadMetaSheet();
+
+            ui.PassNewMetaSheetDatas(datas);
         }
     }
 }
