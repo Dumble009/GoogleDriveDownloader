ba55891aa3678a54bb1006e1a20a1b740e300988
Dumble009 <daichi19990821@gmail.com>
2023-04-22 21:59:09 +0900

    TestUtilをリファクタ
    使用されていなかったプライベートメソッドを呼び出す関数を削除
    代わりに、MetaSheetDataに対するAssert処理を追加した

--- Tests/TestUtil.cs
+++ Tests/TestUtil.cs
@@ -1,5 +1,5 @@
-using System;
-using System.Reflection;
+using NUnit.Framework;
+using GoogleDriveDownloader;
 
 /// <summary>
 /// テストコードが使用する共通処理をまとめた静的クラス
@@ -7,28 +7,16 @@ using System.Reflection;
 public class TestUtil
 {
     /// <summary>
-    /// 任意のオブジェクトのprivateメソッドを実行する
+    /// MetaSheetDataに対してアサート処理を行う
     /// </summary>
-    /// <typeparam name="T">privateメソッドを呼び出したいオブジェクトの型</typeparam>
-    /// <param name="obj">privateメソッドを呼び出したいオブジェクト</param>
-    /// <param name="methodName">呼び出したいprivateメソッドの名称</param>
-    /// <param name="args">渡したい引数の配列。無ければnull</param>
-    /// <returns>実行したprivate関数の返り値。無ければnull</returns>
-    static public object CallPrivateMethod<T>(
-        T obj,
-        string methodName,
-        object[] args
-    )
+    static public void AssertMetaSheetData(
+        MetaSheetData expected,
+        MetaSheetData actual)
     {
-        Type type = typeof(T);
-
-        // T型のmethodNameという関数の情報を取得
-        MethodInfo methodInfo = type.GetMethod(
-            methodName,
-            BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance
-        );
-
-        // 取得した関数を呼び出し
-        return methodInfo.Invoke(obj, args);
+        Assert.AreEqual(expected.ID, actual.ID);
+        Assert.AreEqual(expected.SheetID, actual.SheetID);
+        Assert.AreEqual(expected.SheetName, actual.SheetName);
+        Assert.AreEqual(expected.SavePath, actual.SavePath);
+        Assert.AreEqual(expected.DisplayName, actual.DisplayName);
     }
 }
