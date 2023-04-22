f57eb90341f8863ce0b82b95eecce7909554ec43
Dumble009 <daichi19990821@gmail.com>
2023-04-22 21:08:47 +0900

    UIDrawerクラスの定義を作成

---
+++ Editor/UIs/UIDrawer.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace GoogleDriveDownloader
+{
+    /// <summary>
+    /// 全てのUI要素の描画処理を行うクラス
+    /// </summary>
+    public class UIDrawer
+    {
+        /// <summary>
+        /// 描画対象のUI要素を注入するコンストラクタ
+        /// </summary>
+        /// <param name="_uis">
+        /// 描画対象のUI要素、先頭にあるものから順番に描画命令が発行される
+        /// </param>
+        public UIDrawer(List<IUIElement> _uis)
+        {
+
+        }
+
+        /// <summary>
+        /// 描画命令。コンストラクタで渡されたUI要素を全て描画する
+        /// </summary>
+        public void Draw()
+        {
+
+        }
+    }
+}
