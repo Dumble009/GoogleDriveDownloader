51dc439cd8557e7e4d24f6ceb5d5e71d0ffb34c1
Dumble009 <daichi19990821@gmail.com>
2023-04-23 22:20:04 +0900

    ISheetDataConverterのモッククラスを追加

---
+++ Tests/Mocks/MockSheetDataConverter.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using GoogleDriveDownloader;
+
+/// <summary>
+/// ISheetDataConverterのモッククラス。
+/// Convert関数で引数として渡されたSheetDataオブジェクトを外部に公開し、
+/// 事前に設定されたバイト配列をConvert関数の返り値として返す
+/// </summary>
+public class MockSheetDataConverter : ISheetDataConverter
+{
+    List<byte> convertResult;
+    /// <summary>
+    /// Convert関数の返り値として返されるバイト配列
+    /// </summary>
+    public List<byte> ConvetResult
+    {
+        set => convertResult = value;
+    }
+
+    SheetData passedSheetData;
+    /// <summary>
+    /// Convert関数の引数として渡されたSheetDataオブジェクト
+    /// </summary>
+    public SheetData PassedSheetData
+    {
+        get => passedSheetData;
+    }
+
+    public List<byte> Convert(SheetData sheetData)
+    {
+        passedSheetData = sheetData;
+
+        return convertResult;
+    }
+}
